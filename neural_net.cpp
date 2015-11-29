679de2a895982130995ebab88685f582d8383cd2
Annie <annietupek@gmail.com>
2015-11-28 19:49:50 -0900

    more work on neural net

--- neural_net.cpp
+++ neural_net.cpp
@@ -2,10 +2,27 @@
 
 Neuron::Neuron()
 {
-
+	num_inputs = 0;
+	weights_out = {}; //empty vector
+	output = 0.0;
 }
 
+
+Neuron::Neuron(int _num_inputs, vector<double> _weights_out)
+{
+	num_inputs = _num_inputs;
+	weights_out = _weights_out;
+}
+
+double Neuron::calculate_output()
+{
+	//sum weights coming in
+	return 0.0;
+}
+
+/*
 Neural_Net::Neural_Net()
 {
 
-}
+}*/
+
--- neural_net.h
+++ neural_net.h
@@ -9,19 +9,23 @@ class Neuron
 {
 public:
 	int num_inputs;
-	vector<double> weights;
+	vector<double> weights_out;
 	double output;
 	Neuron();
+	Neuron(int num_inputs, vector<double> weights_out);
+	double calculate_output();
 };
 
 typedef vector<Neuron> neuron_layer;
 
+//don't know if I actually need a neural_net class or if I can just go with the layers of neurons...
+/*
 class Neural_Net
 {
 public:
 	vector<neuron_layer> network;
 	Neural_Net();
 
-};
+};*/
 
 #endif //Neural_Network_Included
